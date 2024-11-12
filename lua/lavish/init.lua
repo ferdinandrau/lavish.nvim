c96cf7e530c7ae3a509a5c11977273387ad74e20
Ferdinand Rau <ferdinandrau@outlook.com>
2024-11-12 13:36:23 +0100

    set terminal colors

--- lua/lavish/init.lua
+++ lua/lavish/init.lua
@@ -28,7 +28,7 @@ M.apply = function(version)
         vim.notify(
             'lavish.nvim in function `apply()`: "'
                 .. version
-                .. '" is not a valid argument. Please use "light", "dark" or `nil`.',
+                .. '" is not a valid argument. Please use "light", "dark" or `nil` (no argument).',
             4
         )
         return
@@ -45,7 +45,32 @@ M.apply = function(version)
         vim.api.nvim_set_hl(0, group, opts)
     end
 
-    -- TODO: set terminal colors
+    local colors = palette.get()[variant]
+
+    if variant == "light" then
+        vim.g.terminal_color_0 = colors.base.fg1
+        vim.g.terminal_color_8 = colors.base.fg2
+        vim.g.terminal_color_7 = colors.base.bg2
+        vim.g.terminal_color_15 = colors.base.bg1
+    else
+        vim.g.terminal_color_0 = colors.base.bg1
+        vim.g.terminal_color_8 = colors.base.bg2
+        vim.g.terminal_color_7 = colors.base.fg2
+        vim.g.terminal_color_15 = colors.base.fg1
+    end
+
+    vim.g.terminal_color_1 = colors.normal.red
+    vim.g.terminal_color_9 = colors.bold.red
+    vim.g.terminal_color_2 = colors.normal.green
+    vim.g.terminal_color_10 = colors.bold.green
+    vim.g.terminal_color_3 = colors.normal.yellow
+    vim.g.terminal_color_11 = colors.bold.yellow
+    vim.g.terminal_color_4 = colors.normal.blue
+    vim.g.terminal_color_12 = colors.bold.blue
+    vim.g.terminal_color_5 = colors.normal.magenta
+    vim.g.terminal_color_13 = colors.bold.magenta
+    vim.g.terminal_color_6 = colors.normal.cyan
+    vim.g.terminal_color_14 = colors.bold.cyan
 end
 
 ---@param opts table|nil
